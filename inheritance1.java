9322c0c53e2c5576d1ffd20828873f9843c5b3e1
Sandesh90288 <pujarisunil7899@gmail.com>
2026-08-31 22:06:31 +0530

    Add Java notes

---
+++ inheritance1.java
@@ -0,0 +1,19 @@
+class Parent {
+
+    static {
+        System.out.println("Parent static");
+    }
+}
+
+class Child extends Parent {
+
+    static {
+        System.out.println("Child static");
+    }
+}
+
+public class inheritance1 {
+    public static void main(String[] args) {
+        Child c = new Child();
+    }
+}
