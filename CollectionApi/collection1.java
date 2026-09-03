6388ba94396f347b133d8291b7a98e40904d048c
Sandesh90288 <pujarisunil7899@gmail.com>
2026-09-03 23:00:01 +0530

    Add CollectionApi1  notes1

--- CollectionApi/collection1.java
+++ CollectionApi/collection1.java
@@ -1,6 +1,8 @@
 package CollectionApi;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class collection1 {
     // lets work with collection interface.Collection is a root interface in
@@ -16,5 +18,17 @@ public class collection1 {
     for(Integer num: nums){
         System.out.println(num);
     }
+    //you can also sort the collection directly using Collections.sort() method
+    //Collections.sort((ArrayList<Integer>) nums);
+    //comparator is an interface which is present in java.util package and it has a method compare() which is used to compare two objects. It is used to sort the collection in ascending or descending order.
+    Comparator<Integer> comparator = new Comparator<Integer>() {
+        @Override
+        //if o1>o2 then return 1, if o1<o2 then return -1, if o1=o2 then return 0
+        public int compare(Integer o1, Integer o2) {
+            return o1.compareTo(o2);
+        }
+    };
+    Collections.sort((ArrayList<Integer>) nums, comparator);
+     
   }
 }
